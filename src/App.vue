220364858c1286a77ed8f96c1a6378791feca550
JonDWesley <jonathan.d.wesley@gmail.com>
2019-08-28 06:24:27 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,113 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavBar/>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
   }
+}
+</script>
+
+<style lang="scss">
+/******************************************************************** 
+START rules that will come before Bulma's variables 
+*********************************************************************/
+@charset "utf-8";
+@import url('https://fonts.googleapis.com/css?family=Nunito:400,700');
+
+// Setting local color scheme
+$white: #fff;
+$black: #000;
+$light-black: hsl(0, 0%, 15%);
+$dark-blue: #092940;
+$light-blue: #397aac;
+$primary: $light-blue;
+$dark-grey: #5f6c7b;
+$light-grey: #ecf0f3;
+
+// Bulma typography variables
+$dark: $dark-blue;
+
+// Bulma menu variables
+$menu-label-color: $light-black;
+$menu-item-color: $light-black;
+$menu-item-active-background-color: $dark-grey;
+$menu-item-hover-color: $white;
+$menu-item-hover-background-color: $light-blue;
+
+// Bulma navbar variables
+$navbar-height: 5rem;
+$navbar-item-img-max-height: 5rem;
+$navbar-background-color: $dark-blue;
+$navbar-item-hover-background-color: $dark-grey;
+/******************************************************************** 
+END rules that will come before Bulma's variables 
+*********************************************************************/
+
+// Import only what you need from Bulma
+@import "../node_modules/bulma/sass/utilities/_all.sass";
+@import "../node_modules/bulma/sass/base/_all.sass";
+
+@import "../node_modules/bulma/sass/elements/box.sass";
+@import "../node_modules/bulma/sass/elements/button.sass";
+@import "../node_modules/bulma/sass/elements/container.sass";
+@import "../node_modules/bulma/sass/elements/content.sass";
+@import "../node_modules/bulma/sass/elements/icon.sass";
+@import "../node_modules/bulma/sass/elements/notification.sass";
+@import "../node_modules/bulma/sass/elements/title.sass";
+@import "../node_modules/bulma/sass/elements/other.sass";
+
+@import "../node_modules/bulma/sass/components/list.sass";
+@import "../node_modules/bulma/sass/components/menu.sass";
+@import "../node_modules/bulma/sass/components/modal.sass";
+@import "../node_modules/bulma/sass/components/navbar.sass";
+
+@import "../node_modules/bulma/sass/layout/hero.sass";
+@import "../node_modules/bulma/sass/layout/section.sass";
+@import "../node_modules/bulma/sass/grid/columns.sass";
+@import "../node_modules/bulma/sass/form/_all.sass";
+
+/******************************************************************** 
+START rules that will come AFTER Bulma's variables 
+*********************************************************************/
+
+// My utility classes
+
+.min-height-full {
+    min-height: 90vh;
+}
+
+.max-height-half {
+    max-height: 70vh;
+}
+
+.is-background-image {
+    background-image: url("../src/assets/background-blue-solid.png");
+}
+
+// Notification styles
+.notification {
+    background-color: $light-blue;
+}
+
+// Side-menu styles
+.side-menu {
+    padding-left: 1rem;
+    background-color: $light-grey;
+    border-radius: 3px;
+    z-index: 1;
+}
+
+// Modal styles
+.modal-close:focus {
+    outline: 2px solid $light-blue;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <nav class="navbar" role="top navigation">
+    <div class="navbar-brand">
+      <div class="navbar-item">
+        <img src="../assets/logo.png" alt="logo">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
+    </div>
+  </nav>
+  <section class="section side-menu column is-2 is-padding-2-top">
+      <nav id="sideMenu" tabindex="0">
+          <p class="menu-label">Contents</p>
+          <ul class="menu-list" role="menu">
+              <li><a href="#" data-selector="instructions" class="is-active" tabindex="-1" role="menuitem">Course Instructions</a></li>
+              <li><a href="#" data-selector="video1" tabindex="-1" role="menuitem">Welcome</a></li>
+              <li><a href="#" data-selector="video2" tabindex="-1" role="menuitem">Meal Count Tools</a></li>
+              <li><a href="#" data-selector="video3" tabindex="-1" role="menuitem">Setting Up</a></li>
+              <li><a href="#" data-selector="video4" tabindex="-1" role="menuitem">Attendance</a></li>
+              <li><a href="#" data-selector="certificate" tabindex="-1" role="menuitem">Quiz and Certificate</a></li>
+          </ul>
+          <p class="menu-label is-padding-2-top">Module Resources</p>
+          <ul class="menu-list">
+              <li><a href="https://www.nutritionnc.com/snp/index.htm/" target="_blank" tabindex="-1">NC CACFP Website</a></li>
+              <li><a href="https://www.nutritionnc.com/snp/pdf/cacfp/forms/fy2019/mealDocCalc/MealCountsandMonthlySummaries/AtRiskAfterschoolDailyRosterandMealCountWorksheet.doc" target="_blank" tabindex="-1">At-risk</a></li>
+              <li><a href="./media/transcript-print.pdf" target="_blank">Download PowerPoint handout</a></li>
+          </ul>
+      </nav>
+  </section>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
