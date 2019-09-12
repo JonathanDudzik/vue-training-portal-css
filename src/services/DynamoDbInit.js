2505bf7e97a4bae411e588dc127609b4cb538143
[Jonathan Dudzik] <[jonathan.dudzik@dhhs.nc.gov]>
2019-09-12 14:51:02 -0400

    fixing dynamodb with cognito

--- src/services/DynamoDbInit.js
+++ src/services/DynamoDbInit.js
@@ -7,6 +7,21 @@ AWS.config.update({
     accessKeyId: "#",
     secretAccessKey: "#"
 });
-var dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  /* 
+     Uncomment the following code to configure Amazon Cognito and make sure to 
+     remove the endpoint, accessKeyId and secretAccessKey specified in the code above. 
+     Make sure Cognito is available in the DynamoDB web service region (specified above).
+     Finally, modify the IdentityPoolId and the RoleArn with your own.
+  */
+/*
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+IdentityPoolId: "us-west-2:12345678-1ab2-123a-1234-a12345ab12",
+RoleArn: "arn:aws:iam::123456789012:role/dynamocognito"
+});
+*/
+
+var dynamodb = new AWS.DynamoDB();
+var docClient = new AWS.DynamoDB.DocumentClient();
 
 export default dynamodb;
