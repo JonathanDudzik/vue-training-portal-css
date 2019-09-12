b115839c75ed6720f8dfbff25b5b20b624258700
[Jonathan Dudzik] <[jonathan.dudzik@dhhs.nc.gov]>
2019-09-12 13:35:22 -0400

    from office

--- src/services/DynamoDbInit.js
+++ src/services/DynamoDbInit.js
@@ -4,8 +4,8 @@ import uuid from 'uuid';
 AWS.config.update({
     region: "us-east-1",
     endpoint: 'dynamodb.us-east-1.amazonaws.com',
-    accessKeyId: "AKIA3ULZLKO5J3BKRYMP",
-    secretAccessKey: "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"
+    accessKeyId: "#",
+    secretAccessKey: "#"
 });
 var dynamodb = new AWS.DynamoDB.DocumentClient();
 
